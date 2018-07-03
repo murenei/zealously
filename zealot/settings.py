7367eb544c6630a149d0eaf2e8be308840d82950
Richard Murray <rick.j.murray@gmail.com>
2018-07-03 18:51:29 +1000

    blah

--- zealot/settings.py
+++ zealot/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
